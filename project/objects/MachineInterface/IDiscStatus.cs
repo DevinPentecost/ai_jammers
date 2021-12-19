d032fbe67c4d5da708e2fa55d5877117e60a121c
Devin Pentecost <devinpentecost@gmail.com>
2021-12-19 02:52:26 -0800

    Rearrange

--- project/objects/MachineInterface/IDiscStatus.cs
+++ project/objects/MachineInterface/IDiscStatus.cs
@@ -1,6 +1,5 @@
 ﻿namespace GodotProject.objects.MachineInterface
 {
-	
 	public enum DiscState
 	{
 		Flying,
@@ -9,12 +8,12 @@
 		Caught,
 		Custom
 	}
-	
+
 	public interface IDiscStatus
 	{
 		float Speed { get; set; } // Units per second
 		float Direction { get; set; } // Radians of current direction
-		float Curve { get; set; }  // Radians per second the disc rotates
+		float Curve { get; set; } // Radians per second the disc rotates
 		DiscState State { get; set; } // Current state of the disc
 	}
 }
--- project/objects/disc/Disc.cs
+++ project/objects/disc/Disc.cs
@@ -1,22 +1,23 @@
-using Godot;
 using System;
+using Godot;
 using GodotProject.objects.MachineInterface;
 
 public class Disc : Area2D, IDiscStatus
 {
-[Export]
-	public DiscState State { get; set; } = DiscState.Flying;
+	[Export] public DiscState State { get; set; } = DiscState.Flying;
+
+	[Export] public float Speed { get; set; } = 100;
+	[Export] public float Direction { get; set; } = 0;
+	[Export] public float Curve { get; set; } = 1;
 
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
 	}
 
 	public override void _PhysicsProcess(float delta)
 	{
-
 		switch (State)
 		{
 			case DiscState.Flying:
@@ -33,25 +34,17 @@ public class Disc : Area2D, IDiscStatus
 			default:
 				throw new ArgumentOutOfRangeException();
 		}
-		
 	}
 
 	private void _ProcessFlyingMovement(float delta)
 	{
 		//First rotate
 		Direction += Curve * delta;
-		
+
 		//Now move in that direction
 		var movement = Vector2.Right.Rotated(Direction) * Speed * delta;
 		var transform2D = Transform;
 		transform2D.origin += movement;
 		Transform = transform2D;
 	}
-
-	[Export]
-	public float Speed { get; set; } = 100;
-	[Export]
-	public float Direction { get; set; } = 0;
-	[Export]
-	public float Curve { get; set; } = 1;
 }
